014c6df89355e08d8d9076656ca2bc9957160d40
Katherine Gollena <katherinegollena7@gmail.com>
2021-12-02 19:55:29 +0800

    added changes to app.js

--- js/app.js
+++ js/app.js
@@ -18,6 +18,41 @@ function loadNames(e) {
     if(origin !== ''){
         url += `region=${origin}&`;
     }
+    // Read the genre and append to the url
+    if(genre !== ''){
+        url += `gender=${genre}&`;
+    }
+    // Read the amount and append to the url
+    if(amount !== ''){
+        url += `amount=${amount}&`;
+    }
 
-    console.log(url);
+    // Ajax Call
+    const xhr = new XMLHttpRequest();
+
+    // Open the connection
+    xhr.open('GET', url, true );
+
+    // Execute the function
+    xhr.onload = function() {
+        if(this.status === 200) {
+            const names = JSON.parse( this.responseText );
+           
+            // Insert into the HTML
+
+            let html = '<h2>Generated Names</h2>';
+            html += '<ul class="list">';
+            names.forEach(function(name){
+                html += `
+                    <li>${name.name}</li>
+                `;
+            });
+            html += '</ul>';
+
+            document.querySelector('#result').innerHTML = html;
+        }
+    }
+
+    // Send the request
+    xhr.send();
 }
